94da449986333a818275bd04d90d79793ffcf124
Bill Ton Hoang Nguyen <bill@birds.ai>
2022-12-04 22:29:34 +0100

    Added 2 buttons, one for email and the other one LinkedIn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
 import react from 'react'
 import './styling/Header.css'
 import profilePic from '../assets/profile_linkedin.jpg'
-// import { Link } from 'react-router-dom';
+import linkedInIcon from '../assets/linkedin_icon.png'
+import emailIcon from '../assets/email_icon.png'
 
 export default function Header() {
     const profilePicStyle = {flex:'1', width: '100%', height: '100%'};
+    const linkedInStyle = {flex:'1', width: '35%', height: '35%'};
+    const emailStyle = {flex:'0.7', width: '20%', height: '20%'};
+
+    function redirectToURL(url) {
+        window.open(url, "_blank");
+    }
 
     return (
         <div className='header'>
@@ -13,6 +20,20 @@ export default function Header() {
             </div>
             <h2>Ton Hoang Nguyen (Bill)</h2>
             <h4 className='job_title'>Software engineer</h4>
+            <div className='buttons'>
+                <button className='email'>
+                    <div className='emailContent'>
+                    <img style={emailStyle} src={emailIcon}></img>
+                    Email
+                    </div>
+                </button>
+                <button className='linkedin' onClick={() => redirectToURL("https://www.linkedin.com/in/bill-n-11a2a0105/")}>
+                    <div className='linkedInContent'>
+                    <img style={linkedInStyle} src={linkedInIcon}></img>
+                    LinkedIn
+                    </div>
+                </button>
+            </div>
             {/* <div>
                 <Link to={{pathname:'https://www.w3schools.com/'}} target="_blank"></Link>
             </div> */}
